d6e69d77850133f7676405f5efe16d655788fa6c
Ruben <19mrs87@gmail.com>
2021-03-06 13:30:40 +0200

    h5

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
 
-<<<<<<< HEAD
+
 console.log('test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
 console.log('test 4');
-=======
+
 console.log('test3');
 console.log('test 444444444444');
->>>>>>> kia
 console.log('test 5');
 console.log('test 7777777');
