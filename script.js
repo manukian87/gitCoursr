c847d113a22e43653a772d8cc9f5aaa650ed56f2
Ruben <19mrs87@gmail.com>
2021-03-06 13:04:05 +0200

    add

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
 
-console.log('test3');
+console.log('test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
 console.log('test 4');
 console.log('test 5');
 console.log('test 7777777');
